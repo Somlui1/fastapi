2478a303ac9bea72e4d27de675f5632cca299ad6
Wajeepradit Prompan (AH) <wajeepradit.p@aapico.com>
2025-08-27 09:36:03 +0700

    update testing patha

--- app/logsdb.py
+++ app/logsdb.py
@@ -1,77 +1,80 @@
-from sqlalchemy import Column, Integer, String, create_engine, text, DateTime
+from sqlalchemy import Column, Integer, String, create_engine, text, DateTime,Date
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func  # สำหรับ func.now()
 
 Base = declarative_base()
 engine = create_engine("postgresql://admin:it%40apico4U@10.10.10.181:5432/license_logsdb")
+#schemas = ["autoform", "nx", "catia", "solidworks","autodesk"]
+schemas = ["testing"]
 
-schemas = ["autoform", "nx", "catia", "solidworks","autodesk"]
-
-# สร้าง schema
 with engine.connect() as conn:
     for schema_name in schemas:
         conn.execute(text(f"CREATE SCHEMA IF NOT EXISTS {schema_name}"))
     conn.commit()
 
-# สร้าง table raws ในแต่ละ schema
 for schema_name in schemas:
     class Log(Base):
         __tablename__ = "raws"
-        __table_args__ = {"schema": schema_name}  # กำหนด schema
+        __table_args__ = {"schema": schema_name} 
 
         id = Column(Integer, primary_key=True)
         name = Column(String)
         data = Column(JSONB)
         created_at = Column(DateTime, server_default=func.now())
 
+class testing(Base):
+   __tablename__ = "users"
+   __table_args__ = {"schema": "testing"} 
+   id = Column(Integer, primary_key=True)
+   email = Column(String)
+   username = Column(String)
+
 # สร้าง table ใน DB
-class nx(Base):
-    __tablename__ = "session_logs"
-    start_date = Column(Date)
-    id = Column(Integer, primary_key=True)
-    start_time = Column(Time)
-    end_date = Column(Date)
-    end_time = Column(Time)
-    duration_minutes = Column(Numeric(10,2))
-    hostname = Column(String)
-    module = Column(String)
-    username = Column(String)
-
-
-class autoform(Base):
-    __tablename__ = "session_logs"
-
-    id = Column(Integer, primary_key=True)
-    start_date = Column(Date)
-    start_time = Column(Time)
-    start_hours = Column(Integer)
-    start_action = Column(String)
-    end_date = Column(Date)
-    end_time = Column(Time)
-    end_hours = Column(Integer)
-    end_action = Column(String)
-    duration_minutes = Column(Numeric(10,2))
-    host = Column(String)
-    module = Column(String)
-    username = Column(String)
-    version = Column(String)
-
-
-class solidwork(Base):
-    __tablename__ = "session_logs"
-
-    id = Column(Integer, primary_key=True)
-    start_date = Column(Date)
-    start_time = Column(Time)
-    end_date = Column(Date)
-    end_time = Column(Time)
-    duration_minutes = Column(Numeric(10,2))
-    feature = Column(String)
-    username = Column(String)
-    computer = Column(String)
-
+#class nx(Base):
+#    __tablename__ = "session_logs"
+#    start_date = Column(Date)
+#    id = Column(Integer, primary_key=True)
+#    start_time = Column(Time)
+#    end_date = Column(Date)
+#    end_time = Column(Time)
+#    duration_minutes = Column(Numeric(10,2))
+#    hostname = Column(String)
+#    module = Column(String)
+#    username = Column(String)
+#
+#
+#class autoform(Base):
+#    __tablename__ = "session_logs"
+#
+#    id = Column(Integer, primary_key=True)
+#    start_date = Column(Date)
+#    start_time = Column(Time)
+#    start_hours = Column(Integer)
+#    start_action = Column(String)
+#    end_date = Column(Date)
+#    end_time = Column(Time)
+#    end_hours = Column(Integer)
+#    end_action = Column(String)
+#    duration_minutes = Column(Numeric(10,2))
+#    host = Column(String)
+#    module = Column(String)
+#    username = Column(String)
+#    version = Column(String)
+#
+#
+#class solidwork(Base):
+#    __tablename__ = "session_logs"
+#
+#    id = Column(Integer, primary_key=True)
+#    start_date = Column(Date)
+#    start_time = Column(Time)
+#    end_date = Column(Date)
+#    end_time = Column(Time)
+#    duration_minutes = Column(Numeric(10,2))
+#    feature = Column(String)
+#    username = Column(String)
+#    computer = Column(String)
 
 Base.metadata.create_all(bind=engine)
-
 print("All schemas and tables created successfully!")
